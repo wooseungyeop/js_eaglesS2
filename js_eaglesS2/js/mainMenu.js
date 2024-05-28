7e5f54ad417adb095a4acb1eaa2ade1400500afc
Stjoo0925 <stjoo0925@gmail.com>
2024-05-29 08:38:28 +0900

    fix : add mainmenu2

--- js_eaglesS2/js/mainMenu.js
+++ js_eaglesS2/js/mainMenu.js
@@ -9,3 +9,38 @@ function Plus() {
 function Del() {
     alert('delete');
 }
+
+function Eat() {
+    alert('먹고가요');
+}
+
+function Packaging() {
+    alert('포장해요');
+}
+function Home(){
+    alert('시작화면으로');
+}
+function Guide() {
+    alert('사용가이드');
+}
+function FoodInfo() {
+    alert('식품정보');
+}
+function Lang() {
+    alert('언어선택');
+}
+function Suggest() {
+    alert('추천메뉴');
+}
+function SetMenu() {
+    alert('세트메뉴');
+}
+function Buger() {
+    alert('단품');
+}
+function Side() {
+    alert('사이드메뉴');
+}
+function Beverage() {
+    alert('음료');
+}
