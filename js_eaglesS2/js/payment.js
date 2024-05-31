b979df07bf79c1b9bd18b01abace6efc1e199992
hwang <hjeonghan0@gmail.com>
2024-05-31 19:34:39 +0900

    hwang

---
+++ js_eaglesS2/js/payment.js
@@ -0,0 +1,24 @@
+function apple(){
+    alert('버튼이 클릭되었습니다');
+}
+function kakao(){
+    alert('버튼이 클릭되었습니다');
+}
+function payko(){
+    alert('버튼이 클릭되었습니다');
+}
+function zero(){
+    alert('버튼이 클릭되었습니다');
+}
+function google(){
+    alert('버튼이 클릭되었습니다');
+}
+function naver(){
+    alert('버튼이 클릭되었습니다');
+}
+function lpay(){
+    alert('버튼이 클릭되었습니다');
+}
+function toss(){
+    alert('버튼이 클릭되었습니다');
+}
