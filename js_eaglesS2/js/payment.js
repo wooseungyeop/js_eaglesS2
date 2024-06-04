d6b6318b8c887f336b4933471165d13529e42f3d
hwang <hjeonghan0@gmail.com>
2024-06-04 17:32:04 +0900

    hwang

--- js_eaglesS2/js/payment.js
+++ js_eaglesS2/js/payment.js
@@ -1,35 +1,78 @@
-function apple(){
-    alert('버튼이 클릭되었습니다');
+function apple() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function kakao(){
-    alert('버튼이 클릭되었습니다');
+function kakao() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function payko(){
-    alert('버튼이 클릭되었습니다');
+function payko() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function zero(){
-    alert('버튼이 클릭되었습니다');
+function zero() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function google(){
-    alert('버튼이 클릭되었습니다');
+function google() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function naver(){
-    alert('버튼이 클릭되었습니다');
+function naver() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function lpay(){
-    alert('버튼이 클릭되었습니다');
+function lpay() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function toss(){
-    alert('버튼이 클릭되었습니다');
+function toss() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
-function payment(){
-    alert('버튼이 클릭되었습니다');
+function payment() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName("popBox")[0];
+  if (element) {
+    element.style.display = "block";
+  }
 }
 
-function kfc(){
-    alert('버튼이 클릭되었습니다');
+function kfc() {
+  alert("버튼이 클릭되었습니다");
+  const element = document.getElementsByClassName('popBox')[0];
+    if (element) {
+        element.style.display = 'block';
+    }
 }
 
 function back(){
-    alert('버튼이 클릭되었습니다');
+    const element = document.getElementsByClassName('popBox')[0];
+    if (element) {
+        element.style.display = 'none';
+    }
 }
